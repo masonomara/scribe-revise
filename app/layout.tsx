072d10ebf744c3a2864b31f1ae5fabe5cdb6e0b8
masonomara <mason.omara@gmail.com>
2024-02-12 11:18:50 -0500

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 const dmsans = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={dmsans.className}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
