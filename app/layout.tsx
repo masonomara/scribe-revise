b3d2160424cffb4867b037774b568e8777c38c73
masonomara <mason.omara@gmail.com>
2024-02-13 12:32:05 -0500

    fixed next-head

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import { Analytics } from "@vercel/analytics/react";
-import Head from "next/head";
 import { Metadata } from "next";
 
 const dmsans = Inter({ subsets: ["latin"] });
@@ -33,38 +32,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <title>ScribeRevise | AI-Powered Revised Messages</title>
-        <meta
-          name="description"
-          content="ScribeRevise | AI-powered web app delivering expert suggestions and revised messages for professional emails, academic papers, marketing content, and more."
-        />
-
-        <meta property="og:url" content="https://scriberevise.com" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="ScribeRevise | AI-Powered Revised Messages"
-        />
-        <meta
-          property="og:description"
-          content="ScribeRevise | AI-powered web app delivering expert suggestions and revised messages for professional emails, academic papers, marketing content, and more."
-        />
-        <meta property="og:image" content="/social.png" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="scriberevise.com" />
-        <meta property="twitter:url" content="https://scriberevise.com" />
-        <meta
-          name="twitter:title"
-          content="ScribeRevise | AI-Powered Revised Messages"
-        />
-        <meta
-          name="twitter:description"
-          content="ScribeRevise | AI-powered web app delivering expert suggestions and revised messages for professional emails, academic papers, marketing content, and more."
-        />
-        <meta name="twitter:image" content="/social.png" />
-      </Head>
       <body className={dmsans.className}>
         <Header />
         {children}
